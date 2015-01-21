71faf585cfcd03e41c5c33b2226852516d219801
jeongHwan <gg7308@bluehole.net>
2015-01-22 02:16:57 +0900

    [Client]VarNode 생성자 및 add 내용 추가

--- Client/PDL/PDL/Factory/NodeType/VarNode.cs
+++ Client/PDL/PDL/Factory/NodeType/VarNode.cs
@@ -8,10 +8,23 @@ namespace PDL.Factory.NodeType
 {
     class VarNode : NodeInterface
     {
-        List<AttributeInterface> childList;
+        public List<AttributeInterface> childList;
+
+        public VarNode()
+        {
+            childList = new List<AttributeInterface>();
+        }
+
         public void add(NodeInterface node)
         {
-
+            if( node is AttributeInterface )
+            {
+                childList.Add(node as AttributeInterface);
+            }
+            else
+            {
+                Console.WriteLine("VarNode에는 Attribute만 입력 가능합니다.");
+            }
         }
         public void exec()
         {
