2107ea29f229ffe5e178265b4bf1b0e008fc9032
jeongHwan <gg7308@bluehole.net>
2015-01-22 02:23:46 +0900

    [Client]AttributeNode 추가

---
+++ Client/PDL/PDL/Factory/NodeType/AttributeNode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDL.Factory.NodeType
+{
+    class AttributeNode<T> : AttributeInterface
+    {
+        public String Key;
+        public T Value;
+
+        public AttributeNode(String Key, T Value)
+        {
+            this.Key = Key;
+            this.Value = Value;
+        }
+
+        public void add(NodeInterface node)
+        {
+            Console.WriteLine("Attribute에는 자식이 있을 수 없습니다.");
+        }
+        public void exec()
+        {
+
+        }
+    }
+}
