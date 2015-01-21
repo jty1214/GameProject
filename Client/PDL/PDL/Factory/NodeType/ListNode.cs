726a49408d8a1c0987497ba91d5acc7b296853c0
BestCard <kiwipie90@naver.com>
2015-01-22 03:28:13 +0900

    ListNode PacketNode..... 맥락이 비슷해 똑같은거 같은 느낌인데 일단 떠오르는게 없어서 이대로 올립니다...

---
+++ Client/PDL/PDL/Factory/NodeType/ListNode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PDL.Factory.Interface;
+
+namespace PDL.Factory.NodeType
+{
+    class ListNode : ChildInterface
+    {
+        public List<NodeInterface> childList;
+
+        public ListNode()
+        {
+            childList = new List<NodeInterface>();
+        }
+
+        public bool add(NodeInterface node)
+        {
+            if (node is ChildInterface)
+            {
+                childList.Add(node as ChildInterface);
+                return true;
+            }
+            if (node is NodeInterface)
+            {
+                childList.Add(node as NodeInterface);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("ListNode에는 Attribute가 입력될 수 없습니다.");
+                return false;
+            }
+        }
+        public void exec()
+        {
+
+        }
+    }
+}
---
+++ Client/PDL/PDL/Factory/NodeType/PacketNode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PDL.Factory.Interface;
+
+namespace PDL.Factory.NodeType
+{
+    class PacketNode : ChildInterface
+    {
+        public List<NodeInterface> childList;
+
+        public PacketNode()
+        {
+            childList = new List<NodeInterface>();
+        }
+
+        public bool add(NodeInterface node)
+        {
+            if( node is ChildInterface )
+            {
+                childList.Add(node as ChildInterface);
+                return true;
+            }
+            if (node is NodeInterface)
+            {
+                childList.Add(node as NodeInterface);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("PacketNode에는 Attribute는 입력할 수 없습니다.");
+                return false;
+            }
+        }
+        public void exec()
+        {
+
+        }
+    }
+}
