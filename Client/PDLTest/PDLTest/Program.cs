5e915a34745c9ea69377767f87b6a4b4428a6a7d
jeongHwan <gg7308@bluehole.net>
2015-01-27 00:35:53 +0900

    [Client] gitignore 깨끗하게 새로 시작, 지울꺼 지우고, 추가할꺼 추가하면서 깃을 최대한 용량을 적게, 자동생성 파일들은 exe와 빌드 전 이벤트를 최대한 활용해서 로드하는 횟수를 적게 관리하자.

--- Client/PDLTest/PDLTest/Program.cs
+++ Client/PDLTest/PDLTest/Program.cs
@@ -11,11 +11,11 @@ namespace PDLTest
         static void Main(string[] args)
         {
             PDL.RESPONSE_FRIEND_LIST_READ pkt = new PDL.RESPONSE_FRIEND_LIST_READ();
-            
-            String[] Name = {"세이라", "카디스 에트라마 디 라이제르", "ㅏㅡㅏㅡㅏㅡㅏㅡㅏㅡㅏㅡ", "Kim John", "John Kim"};
-            Int64[] ServerId = {102938102948901, 120934810293, 12, 012938012938, 34124124142};
-            Int32[] Grade = { 1, 4, 3, 4, 4};
-            Int32[] SubGrade = { 1, 3, 5, 7, 11};
+
+            String[] Name = { "세이라", "카디스 에트라마 디 라이제르", "ㅏㅡㅏㅡㅏㅡㅏㅡㅏㅡㅏㅡ", "Kim John", "John Kim" };
+            Int64[] ServerId = { 102938102948901, 120934810293, 12, 012938012938, 34124124142 };
+            Int32[] Grade = { 1, 4, 3, 4, 4 };
+            Int32[] SubGrade = { 1, 3, 5, 7, 11 };
             for (int i = 0; i < 5; i++)
             {
                 PDL.RESPONSE_FRIEND_LIST_READ.FRIEND_LIST element = new PDL.RESPONSE_FRIEND_LIST_READ.FRIEND_LIST();
@@ -31,7 +31,7 @@ namespace PDLTest
                 Byte[] stream = new Byte[StreamLength];
                 pkt.Serialize(stream);
                 Console.Write("Buffer : ");
-                for(int j=0;j<stream.Length;j++) Console.Write(stream[j] + " ");
+                for (int j = 0; j < stream.Length; j++) Console.Write(stream[j] + " ");
                 Console.WriteLine("");
             }
 
