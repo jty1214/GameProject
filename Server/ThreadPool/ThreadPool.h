73e13b645578486b07d3fb8d512e25f24ecd434c
kimch3617 <kimch3617@naver.com>
2015-03-13 00:20:17 +0900

    function<DWORD()> fuc = bind(맴버 함수, 객체, 인수); 식으로 사용 이 부분을 템플릿화 시키는 것 알아보다 포기 가끔씩 bad_function_call 예외 생김

--- Server/ThreadPool/ThreadPool.h
+++ Server/ThreadPool/ThreadPool.h
@@ -1,14 +1,16 @@
 ﻿#pragma once
+
+#include <iostream>
 #include <functional> 
 #include <windows.h>
 
-#define WORK_MAX  10000
+#define WORK_MAX  7
 #define THREAD_MAX      50
 
 using namespace std;
 using namespace std::placeholders;
 
-typedef DWORD(*WORK) (LPVOID pParam);
+typedef function<DWORD()> WORK;
 
 typedef struct __WorkerThread
 {
@@ -19,14 +21,17 @@ typedef struct __WorkerThread
 typedef struct
 {
 	WORK workList[WORK_MAX];
-	LPVOID pParam[WORK_MAX];
+	//LPVOID pParam[WORK_MAX];
 	DWORD wResult[WORK_MAX];
+	HANDLE circularEvent;
 
 	WorkerThread workerThreadList[THREAD_MAX];
 	HANDLE workerEventList[THREAD_MAX];
 
 	DWORD idxOfCurrentWork;
 	DWORD idxOfLastAddedWork;
+	DWORD cntOfCurrentWork;
+	DWORD cntOfLastAddedWork;
 	// Number of Thread;
 	DWORD threadIdx; // Pool에 존재하는 Thread 의 개수
 }_ThreadPool;
@@ -36,8 +41,8 @@ typedef struct {
 	LPVOID pParam;
 }ThreadParam;
 
-DWORD AddWorkToPool(_ThreadPool *gThreadPool, WORK work, LPVOID pParam);
-DWORD GetWorkFromPool(_ThreadPool *gThreadPool);
+DWORD AddWorkToPool(_ThreadPool *gThreadPool, WORK work);
+int GetWorkFromPool(_ThreadPool *gThreadPool);
 DWORD MakeThreadToPool(_ThreadPool *gThreadPool, DWORD numOfThread);
 void WorkerThreadFunction(LPVOID pParam);
 void initThreadPool(_ThreadPool *gThreadPool);
@@ -86,9 +91,10 @@ printf("Error Occur! \n");
 void initThreadPool(_ThreadPool *gThreadPool) {
 	gThreadPool->idxOfCurrentWork = 0;
 	gThreadPool->idxOfLastAddedWork = 0;
+	gThreadPool->cntOfCurrentWork = 0;
+	gThreadPool->cntOfLastAddedWork = 0;
 	gThreadPool->threadIdx = 0;
-	memset(gThreadPool->pParam, 0, WORK_MAX);
-	memset(gThreadPool->workList, 0, WORK_MAX);
+	gThreadPool->circularEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
 	memset(gThreadPool->wResult, 0, WORK_MAX);
 	memset(gThreadPool->workerEventList, 0, THREAD_MAX);
 	memset(gThreadPool->workerThreadList, 0, THREAD_MAX);
@@ -98,18 +104,19 @@ void initThreadPool(_ThreadPool *gThreadPool) {
 // Thread Pool에 Work 를 등록시키기 위한 함수
 //
 //************************************************************//
-DWORD AddWorkToPool(_ThreadPool *gThreadPool, WORK work, LPVOID pParam)
+DWORD AddWorkToPool(_ThreadPool *gThreadPool, WORK work)
 {
 	//AcquireMutex();
 	if (gThreadPool->idxOfLastAddedWork >= WORK_MAX)
-	{
-		printf("AddWorkToPool fail! \n");
-		return NULL;
+	{		
+		WaitForSingleObject(gThreadPool->circularEvent, INFINITE);
+		gThreadPool->idxOfLastAddedWork = 0;
 	}
 
 	// Work 등ìi록¤I.
-	gThreadPool->workList[gThreadPool->idxOfLastAddedWork] = work;
-	gThreadPool->pParam[gThreadPool->idxOfLastAddedWork++] = pParam;
+	gThreadPool->workList[gThreadPool->idxOfLastAddedWork++] = work;
+	gThreadPool->cntOfLastAddedWork++;
+	//gThreadPool->pParam[gThreadPool->idxOfLastAddedWork++] = pParam;
 
 	// Work 등록 후 대기중인 쓰레드 들을 모두 꺠워 일을 시작하도록 함.
 	// 모두를 깨울 필요 없으므로 정교함이 떨어지는 부분이다.
@@ -126,22 +133,22 @@ DWORD AddWorkToPool(_ThreadPool *gThreadPool, WORK work, LPVOID pParam)
 // Thread에서 Work 을 가져올 때 호출되는 함수
 // gThreadPool에 대한 접근을 보호하기 위해 정의
 //************************************************************//
-DWORD GetWorkFromPool(_ThreadPool *gThreadPool)
+int GetWorkFromPool(_ThreadPool *gThreadPool)
 {
-	WORK work = NULL;
-
-	//AcquireMutex();
-
+	//AcquireMutex();	
 	// 현재 처리해야 할 Work 가 존재하지 않다면..
-	if (!(gThreadPool->idxOfCurrentWork < gThreadPool->idxOfLastAddedWork))
+	if (gThreadPool->idxOfCurrentWork >= WORK_MAX) {
+		SetEvent(gThreadPool->circularEvent);
+		gThreadPool->idxOfCurrentWork = 0;
+	}
+	if (gThreadPool->cntOfCurrentWork >= gThreadPool->cntOfLastAddedWork)
 	{
 		//ReleaseMutex();
 		return -1;
-	}
-
+	}		
 	//work = gThreadPool->workList[gThreadPool->idxOfCurrentWork++];
 	//ReleaseMutex();
-
+	gThreadPool->cntOfCurrentWork++;
 	return gThreadPool->idxOfCurrentWork++;
 }
 
@@ -194,24 +201,35 @@ void WorkerThreadFunction(LPVOID pParam)
 {
 	ThreadParam tp = *(ThreadParam *)pParam;
 	WORK workFunction;
-	LPVOID param;
+	//LPVOID param;
 	HANDLE event = tp.gThreadPool->workerEventList[(DWORD)tp.pParam];	
 	WorkerThread wThread = tp.gThreadPool->workerThreadList[(DWORD)tp.pParam];	
 	while (1)
 	{
 		// work = gThreadPool->workList[gThreadPool->idxOfCurrentWork++];
-		DWORD index = GetWorkFromPool(tp.gThreadPool);
-
-		workFunction = tp.gThreadPool->workList[index];
-		param = tp.gThreadPool->pParam[index];
+		int index = GetWorkFromPool(tp.gThreadPool);		
+		//param = tp.gThreadPool->pParam[index];
 		
 		if (index == -1)
 		{
 			WaitForSingleObject(event, INFINITE);
 			continue;
+		}		
+		workFunction = tp.gThreadPool->workList[index];
+		try {
+			tp.gThreadPool->wResult[index] = workFunction();
 		}
-		tp.gThreadPool->wResult[index] = workFunction(param);
+		catch (const bad_function_call& e) {			
+			cout << index << ": " << e.what() << '\n';
+			//AddWorkToPool(tp.gThreadPool, tp.gThreadPool->workList[index]);
+		}
 	}
 	delete (ThreadParam *)pParam;
 }
-auto asyncWork = bind(AddWorkToPool, _1, _2, _3);
+
+template <typename T, typename ...Args>
+function<DWORD()> asyncWorkInit(_ThreadPool *gThreadPool, T &object, DWORD (T::*mf)(Args...)) {
+	function<DWORD()> fuc = bind((object.*mf), &object, 2);
+	function<DWORD()> asyncWork = bind(AddWorkToPool, gThreadPool, fuc);
+	return asyncWork;
+}
--- Server/ThreadPool/main.cpp
+++ Server/ThreadPool/main.cpp
@@ -2,28 +2,38 @@
 #include <stdlib.h>
 #include "ThreadPool.h"
 
-// Simple Work Function
-DWORD is_prime(int x)
-{
-	Sleep(1000);
-	bool res=false;
-	//printf("Calculating. Please, wait...\n");
-	for (int i = 2; i<x; ++i) if (x%i == 0) res=false;
-	res=true;
-	if (res)
-		printf("%d: %d is prime\n", GetCurrentThreadId(), x);
-	else
-		printf("%d: %d is not prime\n", GetCurrentThreadId(), x);
-	return x;
-}
+class user {
+	// Simple Work Function
+public:
+	DWORD is_prime2(int x, int y)
+	{
+		Sleep(1000);
+		bool res = false;
+		//printf("Calculating. Please, wait...\n");
+		for (int i = 2; i < x; ++i) if (x%i == 0) res = false;
+		res = true;
+		if (res)
+			printf("%d: %d is prime\n", GetCurrentThreadId(), x+y);
+		else
+			printf("%d: %d is not prime\n", GetCurrentThreadId(), x+y);
+		return x;
+	}
+};
 
 int main(int argc, char* argv[])
 {
+	user u;
 	_ThreadPool *gThreadPool = new _ThreadPool;
 	initThreadPool(gThreadPool);
 	MakeThreadToPool(gThreadPool, 3);
-	for (int i = 0; i < 100; i++)
-		asyncWork(gThreadPool, (WORK)is_prime, (LPVOID)i);	
+
+	//======
+	function<DWORD()> fuc = bind(&user::is_prime2, &u, 2, 3);
+	auto asyncWork = bind(AddWorkToPool, _1, fuc);
+	//======
+	//auto asyncWork = asyncWorkInit(gThreadPool, u, &user::is_prime2);
+	for (int i = 0; i < 21; i++)
+		asyncWork(gThreadPool);		
 	Sleep(50000);  // main 쓰레드가 먼저 소멸되는 현상을 방지하기 위해
 
 	return 0;
